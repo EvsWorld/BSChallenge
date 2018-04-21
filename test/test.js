27d7faa36d8cd49ecf0577ee3a1023cf4e4d082f
dromomaniac <evanlhendrix@gmail.com>
2018-04-21 03:42:56 +0200

    take out console.logs

--- test/test.js
+++ test/test.js
@@ -9,7 +9,6 @@ const multiplyAndSum = require('../index.js').multiplyAndSum;
  describe('max', function () {
    it('should maximize the result when arbitray pairs are taken, then the two numbers in the pairs multiplied pairwise, then the products are summed.', function () {
      max([0,1,3,8,4,7,9,9]).should.equal(150);
-     // console.log('max([0,1,3,8,4,7,9,9]) : ', max([0,1,3,8,4,7,9,9]))
      max([0,1,2,3,4,5,6,7,8]).should.equal(101);
      max([0,6,2,3,4,5,1,7,8]).should.equal(101);
      max([0,1,2,3,4,5]).should.equal(27);
@@ -18,7 +17,3 @@ const multiplyAndSum = require('../index.js').multiplyAndSum;
      max([1,1]).should.equal(2);
    });
  });
-
-
- // console.log('max[0,1,2,3,4,5]) =', max([0,1,2,3,4,5]));
- // console.log('multiplyAndSum([[2,3],[4,5]]): ', multiplyAndSum([[2,3],[4,5]]));
